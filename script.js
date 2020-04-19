56d630ca0f5906414778d29c60da365a43ca3690
Thanasis Troboukis <troboukis@gmail.com>
2020-04-19 23:42:33 +0300

    mouse move - enter

--- script.js
+++ script.js
@@ -31,12 +31,39 @@ let screenLog = document.querySelector('#screen-log');
 document.addEventListener('mousemove', logKey);
 
 const SCREENLOG = document.querySelector('#screenLog');
-SCREENLOG.addEventListener('mouseenter', logKey, false);
-SCREENLOG.addEventListener('mouseleave', function(){SCREENLOG.removeAttribute('#screenLog');}, false);
-
 function logKey(e) {
   screenLog.innerText = `
-    X/Y of the cursor: ${e.clientX}, ${e.clientY}`;
+    Position of the cursor (clientX + clientY): ${e.clientX}, ${e.clientY}`;
 }
+const AREA = document.body;
+const CIRCLE = document.querySelector('.circle')
+
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+
+// ΦΤΙΑΧΝΟΥΜΕ ΤΑ functions
+function mouseCoordinates(e){
+  //get the mouse position
+  var horizontalPosition = windowWidth-e.clientX-26;
+  var verticalPosition = windowWidth-e.clientY-26;
+
+  //set the cicle position based on the one of the mousemove
+  CIRCLE.style.left = horizontalPosition + 'px';
+  CIRCLE.style.top = verticalPosition + 'px';
+}
+
+function changeColorOnTouch(){
+  CIRCLE.style.backgroundColor = 'gray';
+  CIRCLE.style.borderColor = 'yellow';
+}
+
+// ΔΗΜΙΟΥΡΓΟΥΜΕ ΤΑ events (eventListenter)
+//όταν μετακινείται στην AREA που έχουμε ορίσει:
+AREA.addEventListener('mousemove',mouseCoordinates,false);
+CIRCLE.addEventListener('mouseenter', changeColorOnTouch, false);
+CIRCLE.addEventListener('mouseleave', function(){CIRCLE.removeAttribute('style');},false);
+
+
+
 // When the mouse leaves the circle, remove inline styles with an anonymous function.
-CIRCLE.addEventListener('mouseleave', function(){CIRCLE.removeAttribute("style");}, false);
+// CIRCLE.addEventListener('mouseleave', function(){CIRCLE.removeAttribute("style");}, false);
